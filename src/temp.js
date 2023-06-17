2632fba5a8ab0290f435406f7b6cad569c794840
Rupesh Prajapati <rupesh_p@ch.iitr.ac.in>
2023-06-17 11:32:07 +0530

    testp

---
+++ src/temp.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import './temp.css';
+export default function Temp() {
+  return (
+    <div className='main'>
+      <img src="https://images.unsplash.com/photo-1641659671200-250fdb343b97?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8M3x8a29yc3xlbnwwfHwwfHw%3D&w=1000&q=80" alt="nahi" />
+      <div className='div2'>
+        <h1 className='perfume'>PERFUMES</h1>
+        <h3 className='desc'>SMELL LIKE YOUR FAVOURITE BOOKS</h3>
+        <button className='butt'>SHOP NOW </button>
+        <p> <span className='star'>&#9733;</span><span className='star'>&#9733;</span><span className='star'>&#9733;</span><span className='star'>&#9733;</span><span className='star'>&#9733;</span>   RATED 4.9/5.0 BY 350 HAPPY CUSTOMERS</p>
+        
+        </div>
+    </div>
+  )
+}
